d86a1fb183e06aec5bda6972d5e0081cf2517bde
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2022-11-24 15:46:57 +0900

    add page (top, signup)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def create
+    render("users/new")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/create" => "users#create"
+  get "/" => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
